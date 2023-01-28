a542bce874e3ba100b07d29f1f6e568851983c26
Igor <>
2023-01-28 19:24:38 +0100

    Add tx addresses sync

---
+++ app/Console/Commands/Syncers/Blockchain/Litecoin/TransactionAddresses.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Console\Commands\Syncers\Blockchain\Litecoin;
+
+use App\Exceptions\Services\Sync\Blockchain\Litecoin\FullSync\BlockAlreadySynced;
+use App\Services\Litecoin\Syncers\TransactionAddressesSync;
+use Denpa\Bitcoin\Client;
+use Illuminate\Console\Command;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
+
+class TransactionAddresses extends Command
+{
+    private const SLEEP_ON_NO_BLOCK = 5;
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ltc:tx-addresses {--from=} {--to=} {--chunk=10}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+
+    protected Client $wallet;
+
+    private \App\Services\Litecoin\Syncers\FullSync $fullSync;
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $fromBlock = $this->option('from');
+        $toBlock = $this->option('to');
+        $chunk = (int)$this->option('chunk');
+
+        $optionCheck = (!isset($fromBlock) && !isset($toBlock)) || ((isset($fromBlock) || isset($toBlock)) && $fromBlock < $toBlock);
+
+        if (!$optionCheck) {
+            $this->error('Invalid period');
+            exit;
+        }
+
+        $syncer = new TransactionAddressesSync();
+
+        $blockToSync = $fromBlock ?? $syncer->getLastSyncedBlockNumber() + 1;
+        $toBlock = $toBlock ?? $syncer->getMaximumBlockNumber();
+        while (true) {
+            if (isset($toBlock) && $blockToSync > $toBlock) {
+                break;
+            }
+
+            $t = microtime(true);
+
+            $this->info(sprintf('Handle blocks from %s to %s', $blockToSync, $chunk));
+            DB::beginTransaction();
+
+            try {
+                $insertedRows = $syncer->syncBlock($blockToSync, $chunk);
+            } catch (\Exception $exception) {
+                DB::rollBack();
+                throw $exception;
+            }
+
+            $this->info(sprintf('Blocks synced in %s Rows: %s', (microtime(true) - $t), $insertedRows));
+            DB::commit();
+            $blockToSync += $chunk;
+        }
+    }
+}
---
+++ app/Services/Litecoin/Syncers/TransactionAddressesSync.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Services\Litecoin\Syncers;
+
+use Illuminate\Support\Facades\DB;
+
+class TransactionAddressesSync
+{
+    public function getLastSyncedBlockNumber(): int
+    {
+        return (int)DB::table('litecoin_transactions_addresses')
+            ->max('block_number');
+    }
+
+    public function getMaximumBlockNumber(): int
+    {
+        return (int)DB::table('litecoin_transactions')
+            ->max('block_number');
+    }
+
+    public function syncBlock($blockNumber, $chunk)
+    {
+        $toBlock = $blockNumber + $chunk - 1;
+        DB::insert('INSERT IGNORE INTO litecoin_transactions_addresses
+                select lto.address, lt.hash, lt.block_number from litecoin_transactions as lt
+                left join litecoin_transaction_outputs as lto on lt.hash = lto.`transaction_hash`
+                where lt.block_number BETWEEN ? AND ?
+                union
+                select lto2.address, lt.hash, lt.block_number from litecoin_transactions as lt
+                left join litecoin_transaction_outputs as lto2 on lt.hash = lto2.`input_transaction_hash`
+                where lt.block_number BETWEEN ? AND ? having address is not null;', [$blockNumber, $toBlock, $blockNumber, $toBlock]);
+    }
+}
